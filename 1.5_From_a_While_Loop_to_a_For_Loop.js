9c26b717a5f6c05e10ce3a260aca44396824b394
Yastrenky <ybramos2014@gmail.com>
2017-05-15 19:47:04 +0100

    first commit

---
+++ 1.5_From_a_While_Loop_to_a_For_Loop.js
@@ -0,0 +1,10 @@
+// Video 1.4 accompanies this exercise
+
+// Write a basic for loop that prints to the console all
+// the numbers from 10 to 1 in descending order. This is the similar to one
+// of the previous challenges, but this time we’re using a for loop instead
+// of a while loop.
+for(let number = 1 ; number <= 10 ; number++){
+console.log(number);
+
+}
